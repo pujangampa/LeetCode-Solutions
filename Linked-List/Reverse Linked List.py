e786a56f270d86bcb1efd48c440fbb165b686008
Pujan <130989697+pujangampa@users.noreply.github.com>
2023-07-24 10:23:40 +0530

    Update Reverse Linked List.py

--- Linked-List/Reverse Linked List.py
+++ Linked-List/Reverse Linked List.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        newlist=None
+        curr=head
+        while curr:
+             nextnode=curr.next
+             curr.next=newlist
+             newlist=curr
+             curr=nextnode
+        return newlist
