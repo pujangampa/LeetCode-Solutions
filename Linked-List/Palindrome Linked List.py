1ae3d3875fc2b4f642027dc4d357212a74072192
Pujan <130989697+pujangampa@users.noreply.github.com>
2023-07-26 12:00:54 +0530

    Create  Palindrome Linked List.py

---
+++ Linked-List/Palindrome Linked List.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        l=[]
+        while head:
+            l.append(head.val)
+            head=head.next
+            
+        return l==l[::-1]
